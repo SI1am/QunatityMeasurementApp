9d1dcb85679def910961c0e7361007418a378899
Shivam <sshivam.mpi0123@gmail.com>
2026-02-18 16:36:36 +0530

    [Shivam] Add. completed UC1 of QuantityMeasurementApp along with unit testing

---
+++ com.QuantityMeasurementApp/src/main/java/QuantityMeasurementApp/com/QuantityMeasurementApp/App.java
@@ -0,0 +1,13 @@
+package QuantityMeasurementApp.com.QuantityMeasurementApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
